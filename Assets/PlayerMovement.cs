6ade753770c9655a0a38a128656684c095513850
maja345eee <121087018+maja345eee@users.noreply.github.com>
2023-01-11 13:28:05 +0100

    commit fix kanske

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 5;
+    public Rigidbody2D myRigidbody;
+    public float jumpStrength;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            transform.position += Vector3.up * jumpStrength * Time.deltaTime;
+        }
+       
     }
 }
